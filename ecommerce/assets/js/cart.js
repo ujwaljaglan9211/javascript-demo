b360e4a2252d0e2f122544ff2c5da6bf8ce2d32f
ujwaljaglan9211 <ujwaljaglan9211@gmail.com>
2021-02-11 17:41:29 +0530

    Razorpay, register, signup, product listing dynamic done

---
+++ ecommerce/assets/js/cart.js
@@ -0,0 +1,144 @@
+// wishlist & cartlist empty array initialized 
+var wishlist = [];
+var payments = [];
+// add item to wishlist starts here
+function addWishlist(obj) {
+  $("#paymentclick").show();
+  $("#pay-now span").text("");
+  document.getElementById("go-to-wishlist").style.display = 'inline-block';
+  document.getElementById("wishlist").style.display = 'block';
+  document.getElementById("total-amount").style.display = 'block';
+  var temp = {};
+  var check;
+  var product_id = obj.getAttribute("product-id");
+  var product_name = obj.getAttribute("product-name");
+  var product_price = obj.getAttribute("product-price");
+  var product_image = obj.getAttribute("product-image");
+  var product_quantity = 1;
+  var product_ammount = product_price * product_quantity;
+  temp.id = product_id;
+  temp.name = product_name;
+  temp.image = product_image;
+  temp.price = product_price;
+  temp.quantity = product_quantity;
+  temp.ammount = product_ammount;
+  if (wishlist.length === 0) {
+    wishlist.push(temp);
+  } else {
+    for (var index = 0; index < wishlist.length; ++index) {
+      if (wishlist[index]['id'] === product_id) {
+        wishlist[index]['quantity']++;
+        wishlist[index]['ammount'] = wishlist[index]['price'] * wishlist[index]['quantity'];
+        check = true;
+        break;
+      }
+    }
+    if (!check) {
+      wishlist.push(temp);
+    }
+  }
+  appendWishlist(wishlist);
+  return false;
+}
+// append items to wishlist starts here
+function appendWishlist(wishlist) {
+  var total_ammount = 0;
+  var items = wishlist.length;
+  var ulShopping = document.getElementsByClassName("shopping-cart-items");
+  var lis = document.querySelectorAll('.shopping-cart-items li');
+  for (var i = 0; li = lis[i]; i++) {
+    li.parentNode.removeChild(li);
+  }
+  ulShopping.innerHTML = '';
+  var total_items = 0;
+  for (var i = 0; i < items; i++) {
+    total_items = total_items + (wishlist[i].quantity);
+  }
+  var tbodyRef = document.getElementById('product-listing').getElementsByTagName('tbody')[0];
+  tbodyRef.innerHTML = '';
+  for (var i = wishlist.length - 1; i >= 0; i--) {
+    var row = tbodyRef.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);
+    cell1.innerHTML = items;
+    cell2.innerHTML = wishlist[i]['name'];
+    cell3.innerHTML = wishlist[i]['price'];
+    cell4.innerHTML = wishlist[i]['quantity'];
+    cell5.innerHTML = wishlist[i]['ammount'];
+    cell6.innerHTML = "<a href='javascript:void(0);'><i class='fa fa-plus' aria-hidden='true' onclick='addMoreWishlist(this); return false;' p-id='" + wishlist[i]['id'] + "'></i></a>";
+    cell7.innerHTML = "<a href='javascript:void(0);'><i class='fa fa-minus' aria-hidden='true' onclick='removeOneWishlist(this); return false;' p-id='" + wishlist[i]['id'] + "'></i></a>";
+    cell8.innerHTML = "<a href='javascript:void(0);'><i class='fa fa-trash' aria-hidden='true' onclick='deleteOneWishlist(this); return false;' p-id='" + wishlist[i]['id'] + "'></i></a>";
+    items--;
+  }
+  for (var i = 0; i < wishlist.length; i++) {
+    total_ammount = total_ammount + wishlist[i]['ammount'];
+    var li = document.createElement("li");
+    li.className = 'clearfix';
+    var img = document.createElement("img");
+    img.src = wishlist[i]['image'];
+    img.alt = wishlist[i]['name'];
+    var spanName = document.createElement("span");
+    spanName.className = 'item-name';
+    spanName.innerHTML = wishlist[i]['name'];
+    var spanPrice = document.createElement("span");
+    spanPrice.className = 'item-price';
+    spanPrice.innerHTML = '₹ ' + wishlist[i]['ammount'];
+    var spanQuantity = document.createElement("span");
+    spanQuantity.className = 'item-quantity';
+    spanQuantity.innerHTML = 'Quantity: ' + wishlist[i]['quantity'];
+    li.appendChild(img);
+    li.appendChild(spanName);
+    li.appendChild(spanPrice);
+    li.appendChild(spanQuantity);
+    $(".shopping-cart-items").append(li);
+    // ulShopping.appendChild(li);
+  }
+  document.getElementsByClassName("shopping-cart-items-listing")[0].childNodes[1].getElementsByClassName("badge").item(0).innerHTML = total_items;
+  document.getElementsByClassName("shopping-cart-total")[0].childNodes[3].innerHTML = '₹ ' + total_ammount;
+  document.getElementById("total-amount").childNodes[1].innerHTML = '₹ ' + total_ammount;
+}
+// addMoreWishlist starts here
+function addMoreWishlist(obj) {
+  var p_id = obj.getAttribute('p-id');
+  for (var index = 0; index < wishlist.length; ++index) {
+    if (wishlist[index]['id'] === p_id) {
+      wishlist[index]['quantity']++;
+      wishlist[index]['ammount'] = wishlist[index]['price'] * wishlist[index]['quantity'];
+    }
+  }
+  appendWishlist(wishlist);
+  return false;
+}
+// removeOneWishlist starts here
+function removeOneWishlist(obj) {
+  var p_id = obj.getAttribute('p-id');
+  for (var index = 0; index < wishlist.length; ++index) {
+    if (wishlist[index]['id'] === p_id) {
+      if (wishlist[index]['quantity'] > 1) {
+        wishlist[index]['quantity']--;
+        wishlist[index]['ammount'] = wishlist[index]['price'] * wishlist[index]['quantity'];
+      } else {
+        wishlist.splice(index, 1);
+      }
+    }
+  }
+  appendWishlist(wishlist);
+  return false;
+}
+// deleteOneWishlist starts here
+function deleteOneWishlist(obj) {
+  var p_id = obj.getAttribute('p-id');
+  for (var index = 0; index < wishlist.length; ++index) {
+    if (wishlist[index]['id'] === p_id) {
+      wishlist.splice(index, 1);
+    }
+  }
+  appendWishlist(wishlist);
+  return false;
+}
--- ecommerce/assets/js/main.js
+++ ecommerce/assets/js/main.js
@@ -28,27 +28,7 @@
     $('html, body').animate({ 
         scrollTop: scrollPosition 
     }, animationDuration); 
-  }); 
-  // products slider (uses the Owl Carousel library)
-  $(".all-products-slider").owlCarousel({
-    autoplay: true,
-    infinite: true,
-    margin:20,
-    dots: true,
-    autoplayHoverPause:true,
-    loop: true,
-    slideSpeed: 1000,
-    paginationSpeed: 500,
-    autoplaySpeed:2500,
-    navigation: true,
-    paginationNumbers: true,
-    nav: true,
-    navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    responsive: { 0: { items: 1 }, 768: { items: 3 }, 900: { items: 4 }
-    }
   });
-  $( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
-  $( ".owl-next").html('<i class="fa fa-chevron-right"></i>');
   
   // Mobile Navigation
   if ($('.main-nav').length) {
@@ -84,4 +64,229 @@
   } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
+
+  $(document).ready(function() {   
+    // Get products from json
+    $.ajax({
+      url: 'assets/data/data.json',
+      dataType: 'json',
+      success: function(data) {
+        // All products slider 
+        $('#load-all-products').html('<div id="all-products-slider" class="products-slider owl-carousel owl-theme"></div>');
+        $.each(data, function (key, value){
+          $('#all-products-slider').append('<div class="item"><div class="product"><img src="'+ value.itemImage+'" alt="'+ value.itemName+'" title="'+ value.itemName+'" /><div class="product-description d-inline-block pt-3 pb-2 text-center"><h5>'+ value.itemName+'</h5><p>Price: '+ value.itemPrice+'/kg</p><a href="javascript:void(0);" class="btn button" onclick="addWishlist(this); return false;" product-id="'+ value.id+'" product-name="'+ value.itemName+'" product-image="'+ value.itemImage+'" product-price="'+ value.itemPrice+'" product-category="'+ value.category+'">Add</a></div></div></div>');
+        });
+        // products slider (uses the Owl Carousel library)
+        $("#all-products-slider").owlCarousel({
+          autoplay: true,
+          infinite: true,
+          margin:20,
+          dots: true,
+          autoplayHoverPause:true,
+          loop: true,
+          slideSpeed: 1000,
+          paginationSpeed: 500,
+          autoplaySpeed:2500,
+          navigation: true,
+          paginationNumbers: true,
+          nav: true,
+          navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+          responsive: { 0: { items: 1 }, 768: { items: 3 }, 900: { items: 4 }
+          }
+        });
+        // Fruits slider
+        $('#load-all-fruits').html('<div id="all-fruits-slider" class="products-slider owl-carousel owl-theme"></div>');
+        $.each(data, function (key, value){
+          if(value.category === 'fruit'){
+            $('#all-fruits-slider').append('<div class="item"><div class="product"><img src="'+ value.itemImage+'" alt="'+ value.itemName+'" title="'+ value.itemName+'" /><div class="product-description d-inline-block pt-3 pb-2 text-center"><h5>'+ value.itemName+'</h5><p>Price: '+ value.itemPrice+'/kg</p><a href="javascript:void(0);" class="btn button" onclick="addWishlist(this); return false;" product-id="'+ value.id+'" product-name="'+ value.itemName+'" product-image="'+ value.itemImage+'" product-price="'+ value.itemPrice+'" product-category="'+ value.category+'">Add</a></div></div></div>');
+          }           
+        });
+        // products slider (uses the Owl Carousel library)
+        $("#all-fruits-slider").owlCarousel({
+          autoplay: true,
+          infinite: true,
+          margin:20,
+          dots: true,
+          autoplayHoverPause:true,
+          loop: true,
+          slideSpeed: 1000,
+          paginationSpeed: 500,
+          autoplaySpeed:2500,
+          navigation: true,
+          paginationNumbers: true,
+          nav: true,
+          navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+          responsive: { 0: { items: 1 }, 768: { items: 3 }, 900: { items: 4 }
+          }
+        });
+        // Vegetables slider
+        $('#load-all-vegetables').html('<div id="all-vegetables-slider" class="products-slider owl-carousel owl-theme"></div>');
+          $.each(data, function (key, value){
+            if(value.category === 'vegetable'){
+              $('#all-vegetables-slider').append('<div class="item"><div class="product"><img src="'+ value.itemImage+'" alt="'+ value.itemName+'" title="'+ value.itemName+'" /><div class="product-description d-inline-block pt-3 pb-2 text-center"><h5>'+ value.itemName+'</h5><p>Price: '+ value.itemPrice+'/kg</p><a href="javascript:void(0);" class="btn button" onclick="addWishlist(this); return false;" product-id="'+ value.id+'" product-name="'+ value.itemName+'" product-image="'+ value.itemImage+'" product-price="'+ value.itemPrice+'" product-category="'+ value.category+'">Add</a></div></div></div>');
+            }           
+          });
+          // products slider (uses the Owl Carousel library)
+          $("#all-vegetables-slider").owlCarousel({
+            autoplay: true,
+            infinite: true,
+            margin:20,
+            dots: true,
+            autoplayHoverPause:true,
+            loop: true,
+            slideSpeed: 1000,
+            paginationSpeed: 500,
+            autoplaySpeed:2500,
+            navigation: true,
+            paginationNumbers: true,
+            nav: true,
+            navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            responsive: { 0: { items: 1 }, 768: { items: 3 }, 900: { items: 4 }
+            }
+          });
+        $( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+        $( ".owl-next").html('<i class="fa fa-chevron-right"></i>');
+      },
+      statusCode: {
+        404: function() {
+          console.log('There was a problem with the server.  Try again soon!');
+        }
+      }
+    });
+    // Flip functionality   
+    $("#flip-container").click(function() {
+      $(this).toggleClass("flip-container");
+    });
+    // LocalStorage GET
+    var account = JSON.parse(localStorage.getItem("account"));
+    checkLoggedIn(account);
+    // email validation
+    function validateEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    }
+    // password validation
+    function validatePassword(password) {
+      const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/;
+      return re.test(String(password));
+    }
+    // check whether logged in or not
+    function checkLoggedIn(account){
+      if(account && account.isLoggedIn === true){
+        $("#checkoutForm h4").text("Checkout");
+        $("#signin").hide();
+        $("#registeration").hide();
+        $("#paymentclick").hide();
+      }else if(account && account.isLoggedIn === false){
+        $("#checkoutForm h4").text("Login");
+        $("#pay-now span").text("");
+        $("#signin").show();
+        $("#registeration").hide();
+        $("#paymentclick").hide();
+      }else{
+        $("#pay-now span").text("");
+        $("#signin").hide();
+        $("#registeration").show();
+        $("#paymentclick").hide();
+      }
+    }
+    // Login
+    $('#login').click(function() {
+      console.log('account', account);
+      var emailLogin = $("#emailLogin").val();
+      if(!validateEmail(emailLogin)){
+        $(".emailLoginError").text("Email is invalid!").show().delay(5000).fadeOut();
+      }
+      var passwordLogin = $("#passwordLogin").val();
+      if(!validatePassword(passwordLogin)){
+        $(".passwordLoginError").text("Password is invalid!").show().delay(5000).fadeOut();
+      }
+      if(account.email === emailLogin && account.password === passwordLogin){
+        account.isLoggedIn = true;
+        localStorage.setItem("account", JSON.stringify(account));
+      }else{
+        $("#loginError").text("Either email or password are incorrect!").show().delay(5000).fadeOut();
+      }
+    });
+    // register
+    $('#register').click(function() {
+      var name = $("#name").val();
+      var email = $("#email").val();
+      var phone = $("#phone").val();
+      var address = $("#shippingAddress").val();
+      if(!validateEmail(email)){
+        $(".emailError").text("Email is invalid!").show().delay(5000).fadeOut();
+        return false;
+      }
+      var password = $("#password").val();
+      if(!validatePassword(password)){
+        $(".passwordError").text("Password is invalid!").show().delay(5000).fadeOut();
+        return false;
+      }
+      var account = {};
+      account.name = name;
+      account.email = email;
+      account.phone = phone;
+      account.address = address;
+      account.password = password;
+      account.isLoggedIn = false;
+      if(localStorage.setItem("account", JSON.stringify(account))){
+        $("#registeration").hide();
+        // payment function
+        payment();
+      }
+    });
+    // click of but now button
+    $("#paymentclick").on('click', function(){ 
+      payment();
+    });
+    function payment(){
+      var total_ammount = 0;
+      var description = '';
+      if(wishlist.length>0){
+        for (var i = 0; i < wishlist.length; i++) {
+          total_ammount = total_ammount + wishlist[i]['ammount'];
+          description = description +  wishlist[i]['name'] + "(" +  wishlist[i]['quantity'] + "),";         
+        }
+        // Pay via Razorpay     
+        var options = {
+          "key": "rzp_test_WKwmEQsqHL0Vgj",
+          "amount": total_ammount*100, // Example: 2000 paise = INR 20
+          "name": "iMarket",
+          "description": "Purchase of following products - " + description ,
+          "image": "assets/images/logo.svg",// COMPANY LOGO
+          "handler": function (response) {
+            // AFTER TRANSACTION IS COMPLETE YOU WILL GET THE RESPONSE HERE
+            console.log('response',response);
+            var temp = {};
+            temp.razorpay_payment_id = response.razorpay_payment_id;
+            temp.email = account.email;
+            temp.phone = account.phone;
+            temp.ammount = total_ammount;
+            payments.push(temp);
+            localStorage.setItem("payments", JSON.stringify(payments));
+            wishlist=[];
+            appendWishlist(wishlist);
+          },
+          "prefill": {
+            "name": account.name, // pass customer name
+            "email": account.email,// customer email
+            "contact": account.phone //customer phone no.
+          },
+          "notes": {
+            "address": account.address //customer address 
+          },
+          "theme": {
+            "color": "#52AF3C" // screen color
+          }
+        };
+        console.log('options', options);
+        var propay = new Razorpay(options);
+        propay.open();
+      }else{
+        $("#pay-now span").text("Your cart is empty!");
+        $("#paymentclick").hide();
+      }
+    }
+ });
 })(jQuery);
